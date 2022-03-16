3499e22500baba70a530d32f25706653c2e0026e
alkenny-21 <kennkyragu@gmail.com>
2022-03-15 19:22:08 -0700

    new changes

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - Shows 1 if the input is a
- * letter, lowercase, or uppercase
+ * letter Another cases, shows 0
  *
  * @c: The character in ASCII code
  *
@@ -10,13 +10,13 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c>= 65 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 	_putchar('\n');
 }
